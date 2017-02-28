d399ce55b9d178e9597111bc59a886efe6dad1e8
3ll3n <ellengrafton01@gmail.com>
2017-02-28 20:34:59 +0000

    Added Calculator Activity and layout

--- app/src/main/java/com/codeclan/example/calculator/CalculatorActivity.java
+++ app/src/main/java/com/codeclan/example/calculator/CalculatorActivity.java
@@ -1,13 +1,33 @@
 package com.codeclan.example.calculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CalculatorActivity extends AppCompatActivity {
+    EditText firstNumEditText;
+    EditText secondNumEditText;
+    Button shakeButton;
+    Calculator calc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        firstNumEditText = (EditText)findViewById(R.id.num1_edittext);
+        secondNumEditText = (EditText)findViewById(R.id.num2_edittext);
+        shakeButton = (Button)findViewById(R.id.shake_btn);
+
+        calc = new Calculator();
+    }
+
+    public void onShakeButtonClicked(View button) {
+//        int answer = calc.addNumbers();
+//
+//        Intent intent = new Intent(CalculatorActivity.this, AnswerActivity.class)
     }
 }
